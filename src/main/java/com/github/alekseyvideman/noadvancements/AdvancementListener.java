0eaa9646903068147f84d3614a5c9baa6955e8c4
AlekseyVideman <alekseyvideman@gmail.com>
2024-05-24 21:31:43 +0700

    Added random messages feature

--- src/main/java/com/github/alekseyvideman/noadvancements/AdvancementListener.java
+++ src/main/java/com/github/alekseyvideman/noadvancements/AdvancementListener.java
@@ -2,21 +2,25 @@ package com.github.alekseyvideman.noadvancements;
 
 import com.destroystokyo.paper.event.player.PlayerAdvancementCriterionGrantEvent;
 import net.kyori.adventure.text.Component;
+import org.bukkit.advancement.Advancement;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 
-public class AdvancementListener implements Listener {
-    private final NoAdvancements pl;
+import java.util.Random;
 
-    public AdvancementListener(NoAdvancements pl) {
-        this.pl = pl;
+public class AdvancementListener implements Listener {
+
+    private final NoAdvancements plugin;
+
+    public AdvancementListener(NoAdvancements plugin) {
+        this.plugin = plugin;
     }
 
     @EventHandler
     public void saveAdvancement(PlayerAdvancementCriterionGrantEvent e) {
         var playerWorld = e.getPlayer().getWorld().getName();
-        var blockedWorlds = pl.getConfig().getStringList("blocked-worlds");
+        var blockedWorlds = plugin.getConfig().getStringList("blocked-worlds");
         for (var world : blockedWorlds) {
             if (!(world.equals(playerWorld))) {
                 return;
@@ -27,20 +31,28 @@ public class AdvancementListener implements Listener {
 
     @EventHandler
     public void broadcastAdvancement(PlayerAdvancementDoneEvent e) {
-        var doBroadcast = pl.getConfig().getBoolean("broadcast-advancement-message");
-        if (doBroadcast) {
+        var broadcastVanillaMessage = plugin.getConfig().getBoolean("broadcast-advancement-message");
+        if (broadcastVanillaMessage) {
             return;
         }
         if (e.getAdvancement().getDisplay() == null) {
             return;
         }
+        e.message(null); // Cancel vanilla message sending
 
         var player = e.getPlayer();
-        var firstPartResultMessage = pl.getConfig().getString("advancement-message");
-        var displayName = e.getAdvancement().displayName();
-        var resultMessage = Component.text(firstPartResultMessage).append(displayName);
-        player.sendMessage(resultMessage);
+        player.sendMessage(getAdvancementMessage(e.getAdvancement()));
+    }
 
-        e.message(null); // Cancel vanilla message sending
+    private Component getAdvancementMessage(Advancement advancement) {
+        System.out.println(advancement.getKey());
+        var advancementMessages = plugin.getConfig().getStringList("advancement-message");
+        var advancementMessage = advancementMessages.get(new Random().nextInt(advancementMessages.size()));
+
+        return Component.text()
+                .append(Component.text(advancementMessage))
+                .append(Component.space())
+                .append(advancement.getDisplay().displayName())
+                .build();
     }
 }
